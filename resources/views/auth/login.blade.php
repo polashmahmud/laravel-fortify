ddc1a8a4894df56d251425333cc46b7b0e8ef9ce
Polash Mahmud <polashmahmud@gmail.com>
2023-05-26 15:35:05 +0600

    Register & Login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,28 @@
+<x-app-layout>
+    <form action="{{ route('login') }}" method="post">
+        @csrf
+
+        <div>
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" value="{{ old('email') }}">
+            
+            @error('email')
+                <div>{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div>
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" value="{{ old('password') }}">
+
+            @error('password')
+                <div>{{ $message }}</div>
+            @enderror
+        </div>
+
+
+        <div>
+            <button type="submit">Login</button>
+        </div>
+    </form>
+</x-app-layout>
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,45 @@
+<x-app-layout>
+    <form action="{{ route('register') }}" method="post">
+        @csrf
+
+        <div>
+            <label for="name">Name</label>
+            <input type="text" name="name" id="name" value="{{ old('name') }}">
+
+            @error('name')
+                <div>{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div>
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" value="{{ old('email') }}">
+            
+            @error('email')
+                <div>{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div>
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" value="{{ old('password') }}">
+
+            @error('password')
+                <div>{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div>
+            <label for="password_confirmation">Password Confirmation</label>
+            <input type="password" name="password_confirmation" id="password_confirmation" value="{{ old('password_confirmation') }}">
+
+            @error('password_confirmation')
+                <div>{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div>
+            <button type="submit">Register</button>
+        </div>
+    </form>
+</x-app-layout>
