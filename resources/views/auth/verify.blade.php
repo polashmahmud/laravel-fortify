1cb294b74d315e7eaa46fb24bf4e331ec3e8dd81
Polash Mahmud <polashmahmud@gmail.com>
2023-05-27 16:41:08 +0600

    verify email

---
+++ resources/views/auth/verify.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout>
+    <p>You must verify your account before accessing this.</p>
+
+    @if(session('status') == 'verification-link-sent')
+        <p>A verification email has been send.</p>
+    @endif
+
+    <form action="{{ route('verification.send') }}" method="post">
+        @csrf
+
+        <button type="submit">Resend email</button>
+    </form>
+</x-app-layout>
